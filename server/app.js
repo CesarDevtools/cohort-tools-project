0f25e18a5bb3bc4c7f94ade7b1bbd2a997fc78cf
Cesar <cessardevs@gmail.com>
2026-04-01 14:13:25 +0100

    feat: implement get route for specific project and fixed routed for list of projects

--- server/app.js
+++ server/app.js
@@ -24,48 +24,77 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
-	cors({
-		origin: ["http://localhost:5173"],
-	}),
+  cors({
+    origin: ["http://localhost:5173"],
+  }),
 );
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
-	res.sendFile(__dirname + "/views/docs.html");
+  res.sendFile(__dirname + "/views/docs.html");
 });
+
+
+/********************/
+/***PROJECTS ROUTES**/
+/********************/
 
 app.get("/projects", (req, res) => {
-	Project.find({})
-		.then((projects) => {
-			console.log("Retrieved books ->", projects);
-			res.json(projects);
-		})
-		.catch((error) => {
-			console.error("Error while retrieving books ->", error);
-			res.status(500).json({ error: "Failed to retrieve books" });
-		});
+  Project.find({})
+    .then((projects) => {
+      console.log("Retrieved projects", );
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving projects ->", error);
+      res.status(500).json({ error: "Failed to retrieve projects" });
+    });
 });
 
+app.get("/projects/:projectId", (req, res) => {
+  const { projectId } = req.params;
+
+  Project.findById(projectId)
+    .then((project) => {
+      console.log("Project retrieved");
+      res.status(200).json(project);
+    })
+    .catch((err) => {
+      console.error("Error while retrieving the project /n/n", err);
+      res.status(500).json({ error: "failed to retrieve project" });
+    });
+});
+
+app.get("/projects/:projectId", (req, res) => {});
+
+
+
+/********************/
+/***STUDENTS ROUTES**/
+/********************/
+
+
 app.get("/students", (req, res) => {
-	Student.find({})
-		.then((students) => {
-			console.log("Retrieved books ->", students);
-			res.json(students);
-		})
-		.catch((error) => {
-			console.error("Error while retrieving books ->", error);
-			res.status(500).json({ error: "Failed to retrieve books" });
-		});
+  Student.find({})
+    .populate("projects")
+    .then((students) => {
+      console.log("Retrieved books ->", students);
+      res.json(students);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving books ->", error);
+      res.status(500).json({ error: "Failed to retrieve books" });
+    });
 });
 
 // START SERVER
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 
 mongoose
-	.connect("mongodb://127.0.0.1:27017/cohorts")
-	.then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
-	.catch((err) => console.error("Error connecting to MongoDB", err));
+  .connect("mongodb://127.0.0.1:27017/cohorts")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
